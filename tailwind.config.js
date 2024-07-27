469e62807cc4004257c15ebd347e1e160b87d247
code3-dev <h3dev.pira@gmail.com>
2024-07-27 20:14:58 +0330

    first publish

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {},
+  plugins: [],
+};
+
+// npx tailwindcss -i css/core.css -o css/tailwind.min.css --minify
